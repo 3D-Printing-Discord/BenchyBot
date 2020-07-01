0cb3017955eaa80ada4307d3340840dc2b424c4d
Edward Chamberlain <edward.chamberlain@me.com>
2020-07-01 18:12:28 +0100

    Added reacts, corrected ID, updated message

--- modules/Showcase/Showcase.py
+++ modules/Showcase/Showcase.py
@@ -3,12 +3,15 @@ import discord
 from discord.ext import commands
 import json
 import bot_utils
+import random
 
 class Showcase(commands.Cog):
     version = "v0.1"
 
     def __init__(self, bot):
         self.bot = bot
+
+        self.reacts = ['👍', '❤️', '‼️', '🆒', '💯', '🔥']
 
         self.config_data = []
         with open('modules/Showcase/config.json') as f:
@@ -18,9 +21,10 @@ class Showcase(commands.Cog):
     async def on_message(self, message):
         if message.channel.id in self.config_data['showcase_channels']:
             if not message.attachments:
-                await bot_utils.bot_log(self.bot, f"{message.content} by {message.author} was removed from a showcase channel.")
                 await message.author.send(self.config_data['showcase_message'])
                 await message.delete()
+            else:
+                message.add_reaction(random.choice(self.reacts))
 
 def setup(bot):
     bot.add_cog(Showcase(bot))
