fb89cdcdc92fbb4b9ba054943f875afe9e645f15
Edward Chamberlain <edward.chamberlain@me.com>
2020-07-25 14:07:52 +0100

    added docstring

--- modules/Mute/Mute.py
+++ modules/Mute/Mute.py
@@ -17,7 +17,9 @@ class Mute(commands.Cog):
 
     @commands.command()
     @commands.has_any_role(*bot_utils.admin_roles)
-    async def mute(self, ctx, member: discord.Member, time):
+    async def mute(self, ctx, member: discord.Member, time=None):
+        '''Apply a mute to a member for a set number of mins.'''
+
         try:
             time = float(time)
         except:
@@ -28,10 +30,12 @@ class Mute(commands.Cog):
         embed = discord.Embed(title="Mute", description=f"{member} has been muted for {time} mins.", color=bot_utils.green)
         await ctx.send(embed=embed)
 
+        if time is None:
+            return
+            
         await asyncio.sleep(time * 60)
 
         await member.add_roles(role_obj)
-
         await member.remove_roles(role_obj)
         embed = discord.Embed(title="Mute", description=f"{member} has been unmuted.", color=bot_utils.green)
         await ctx.send(embed=embed)
