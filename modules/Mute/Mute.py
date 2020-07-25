42706ed15f6a67d726bec87abd30916e40e4a053
Edward Chamberlain <edward.chamberlain@me.com>
2020-07-25 14:39:16 +0100

    Added study mute

--- modules/Mute/Mute.py
+++ modules/Mute/Mute.py
@@ -16,6 +16,35 @@ class Mute(commands.Cog):
             self.config_data = json.load(f)
 
     @commands.command()
+    async def study_mute(self, ctx, time=None):
+        '''Applies a study mute to yourself for a set number of mins.'''
+        if time is None:
+            await ctx.send("You forgot to specify how long you want muting for!\nUsage: `?study_mute [time to mute in mins]`")
+            return
+        
+        try:
+            time=float(time)
+        except:
+            await ctx.send("I didnt recognise that time!\nUsage: `?study_mute [time to mute in mins]`")
+            return
+
+        if not await bot_utils.await_confirm(ctx, f"Confirm that you ({ctx.author}) want muting for {time} mins?"):
+            return
+
+        role_obj = ctx.guild.get_role(self.config_data['mute_role'])
+        await ctx.author.add_roles(role_obj)
+        embed = discord.Embed(title="Mute", description=f"{ctx.author} has been muted for {time} mins.", color=bot_utils.green)
+        await ctx.send(embed=embed)
+            
+        await asyncio.sleep(time * 60)
+
+        await ctx.author.add_roles(role_obj)
+        await ctx.author.remove_roles(role_obj)
+        embed = discord.Embed(title="Mute", description=f"{ctx.author} has been unmuted.", color=bot_utils.green)
+        await ctx.send(embed=embed)
+            
+
+    @commands.command()
     @commands.has_any_role(*bot_utils.admin_roles)
     async def mute(self, ctx, member: discord.Member, time=None):
         '''Apply a mute to a member for a set number of mins.'''
@@ -23,16 +52,14 @@ class Mute(commands.Cog):
         try:
             time = float(time)
         except:
-            ctx.send
+            await ctx.send("Error.\nUsage: `?mute [member] [time in mins]")
+            
 
         role_obj = ctx.guild.get_role(self.config_data['mute_role'])
         await member.add_roles(role_obj)
         embed = discord.Embed(title="Mute", description=f"{member} has been muted for {time} mins.", color=bot_utils.green)
         await ctx.send(embed=embed)
 
-        if time is None:
-            return
-            
         await asyncio.sleep(time * 60)
 
         await member.add_roles(role_obj)
