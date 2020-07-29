1473b6f03aa91241136cbd4d4237b652bb356697
Edward Chamberlain <edward.chamberlain@me.com>
2020-07-29 13:49:03 +0100

    added time limit.

--- modules/Moderation/Moderation.py
+++ modules/Moderation/Moderation.py
@@ -30,21 +30,37 @@ class Moderation(commands.Cog):
         
         if member:
             embed.set_author(name=f"{member} please see the rule below:")
-
-        warn_mesg = await ctx.send(member.mention, embed=embed)
-
-        if member:
+            warn_mesg = await ctx.send(member.mention, embed=embed)
             self.c.execute("INSERT INTO Moderation_warnings(timestamp, user_id, reason, jump_link) VALUES (?, ?, ?, ?)", (datetime.datetime.utcnow(), member.id, rule_number, warn_mesg.jump_url))
             self.conn.commit()
             await ctx.guild.get_channel(self.bot.config['bot_log_channel']).send(f"Warning issued to {member}.\n{warn_mesg.jump_url}")
+        else:
+            await ctx.send(embed=embed)
 
     @commands.command()
     @commands.has_any_role(*bot_utils.admin_roles)
-    async def show_infactions(self, ctx, member: discord.Member):
-        self.c.execute("SELECT * FROM Moderation_warnings WHERE user_id=?", (member.id, ))
-        results = self.c.fetchall()
+    async def show_infractions(self, ctx, member: discord.Member, range=None):
+
+        if range is not None:
+            self.c.execute("SELECT * FROM Moderation_warnings WHERE user_id=?", (member.id, ))
+            results = self.c.fetchall()
+        else:
+            search_date = datetime.datetime.utcnow() - datetime.timedelta(days=120)
+            self.c.execute("SELECT * FROM Moderation_warnings WHERE user_id=? AND timestamp > ?", (member.id, search_date))
+            results = self.c.fetchall()
         
-        await ctx.send(f"{len(results)} warning(s) recorded for {member}")
+        inf = {'1':0, '2':0, '3':0, '4':0, '5':0, '6':0, '7':0}
+        for v in results:
+            inf[v[2]] += 1
+
+        output = []
+        for k, v in inf.items():
+            if v != 0:
+                output.append(f"Rule {k}: {v}")
+        
+        out_string = "\n".join(output)
+
+        await ctx.send(f"{len(results)} warning(s) recorded for {member}\n```\n{out_string}```")
 
 def setup(bot):
     bot.add_cog(Moderation(bot))
