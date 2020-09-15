cdf6399b3b59e432ef6de87ecf89a078ec1af3a1
Edward Chamberlain <edward.chamberlain@me.com>
2020-09-15 14:45:43 +0100

    placeholder database creation

---
+++ modules/Mute/init_database.py
@@ -0,0 +1,15 @@
+import sqlite3
+import sys
+
+#DB Name
+conn = sqlite3.connect(sys.argv[1])
+c = conn.cursor()
+
+# Create table
+c.execute('''CREATE TABLE ____ (______ text, ______ integer)''')
+
+# Save (commit) the changes
+conn.commit()
+
+# close connection 
+conn.close()
