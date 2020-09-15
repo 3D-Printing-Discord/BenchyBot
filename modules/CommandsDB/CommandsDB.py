11d5979a3944b4db91fa10e30f030e84ca23e1ce
Edward Chamberlain <edward.chamberlain@me.com>
2020-09-15 14:39:39 +0100

    added get_command

--- modules/CommandsDB/CommandsDB.py
+++ modules/CommandsDB/CommandsDB.py
@@ -22,9 +22,9 @@ class CommandsDB(commands.Cog):
         self.bot = bot
 
         # LOAD CONFIG DATA
-        #self.config_data = []
-        #with open('modules/CommandsDB/config.json') as f:
-        #    self.config_data = json.load(f)
+        self.config_data = []
+        with open('modules/CommandsDB/config.json') as f:
+           self.config_data = json.load(f)
 
         self.conn = sqlite3.connect(self.bot.config['database'])
         self.c = self.conn.cursor()
@@ -83,7 +83,7 @@ class CommandsDB(commands.Cog):
         response = command[1]
         if DEBUG: print("Response = ", response)
 
-        await self.send_command(ctx, response)
+        await self.send_command(ctx, response, command=command)
 
         if DEBUG: print("Done!")
 
@@ -110,7 +110,7 @@ class CommandsDB(commands.Cog):
         await self.add_command(ctx, 'help', command, response)
     
     @commands.command()
-    @commands.check(bot_utils.is_bot_channel)
+    @commands.check(bot_utils.is_secret_channel)
     @commands.has_any_role(*bot_utils.admin_roles)
     async def cc(self, ctx, command=None, *, response=None):
         '''
@@ -145,7 +145,7 @@ class CommandsDB(commands.Cog):
             self.c.execute("SELECT * FROM Commands WHERE command_type='alias'")
             result = self.c.fetchall()
             
-            result.sort(key=lambda x: x[0])
+            result.sort(key=lambda x: x[1])
 
             # CREATE PAGINATOR
             paginator = commands.Paginator(prefix='```\n', suffix='\n```')
@@ -188,6 +188,7 @@ class CommandsDB(commands.Cog):
                 await ctx.send(f"Failed to add '{command}' to databse!")
 
     @commands.command()
+    @commands.check(bot_utils.is_bot_channel)
     async def help_db(self, ctx):
         '''
         Shows all databse commands.
@@ -216,6 +217,21 @@ class CommandsDB(commands.Cog):
 
             loaded_file = discord.File("runtimefiles/Database-CSV.csv", filename="Database-CSV.csv")
             await ctx.send("CSV File of the commands Database.", file=loaded_file)
+
+    @commands.command()
+    @commands.check(bot_utils.is_bot_channel)
+    @commands.has_any_role(*bot_utils.reg_roles)
+    async def get_command(self, ctx, command):
+        '''Gets the plain text version of a command from the database.'''
+        if any(role.id in bot_utils.admin_roles for role in ctx.message.author.roles):
+            command = self.bot.databasehandler.sqlquery("SELECT * FROM Commands WHERE command=?", command, return_type='one')
+        else:
+            command = self.bot.databasehandler.sqlquery("SELECT * FROM Commands WHERE command=? AND command_type=?", command, "help", return_type='one')
+
+        if command is None:
+            await ctx.send("Sorry, didnt find that one!")
+        else:
+            await ctx.send(f"```\n{command[1]}\n```")
 
     @commands.command()
     @commands.check(bot_utils.is_bot_channel)
@@ -269,39 +285,85 @@ class CommandsDB(commands.Cog):
         # SEND PAGINATOR
         for page in paginator.pages:
             await ctx.send(page)
+   
+    @commands_by.error
+    async def commands_by_handler(self, ctx, error):
+        if isinstance(error, commands.MissingRequiredArgument):
+            embed=discord.Embed(title="You need to provide a users name.", description="Use the command like this:\n`?commands_by [User Name]`\n\nExample\n`?commands_by Ed`\n\nYou can find users with either their ID, name or mention.")
+            await ctx.send(embed=embed)
+            ctx.handled_in_local = True
+
+        if isinstance(error, commands.errors.BadArgument):
+            embed=discord.Embed(title="Member Not Found", description="Sorry, I didnt find that user.\nYou can find users with either their ID, name or mention. if their name includes a space you will have to use either the mention or ID.")
+            await ctx.send(embed=embed)
+            ctx.handled_in_local = True
 
     # -- HELPER FUNCTIONS --
+
+    def embed_type(self, input_string):
+        if not input_string.startswith('<'):
+            return False
+        
+        closing_index = input_string.index('>')
+
+        if closing_index == 1:
+            return True
+        else: 
+            return input_string[1:closing_index]
 
     def get_user_commands(self, member):
         self.c.execute("SELECT * FROM Commands WHERE owner=?", (member.id, ))
         result = self.c.fetchall()
         return result
 
-    async def send_command(self, ctx, response, delete=True):
-        if response.startswith("<>"):
-            embed=discord.Embed(description=bot_utils.sanitize_input(response[2:]))
-            embed.set_footer(text=f"Sent by: {ctx.author}")
+    async def send_command(self, ctx, response, command="?", delete=True):
+        command_type = self.embed_type(response)
+
+        if command_type == False:
+            if DEBUG: print("STANDARD")
+            await ctx.send(bot_utils.sanitize_input(response))
+        elif command_type == True:
+            if DEBUG: print("EMBED")
+            embed=discord.Embed(description=bot_utils.sanitize_input(response)[2:])
+            embed.set_footer(text=f"?{command[0]} requested by: {ctx.author}")
             await ctx.send(embed=embed)
             if delete:
                 await ctx.message.delete()
         else:
-            await ctx.send(bot_utils.sanitize_input(response))
+            if DEBUG: print("IMAGE EMBED")
+            response = response[response.index('>')+1:]
+            if DEBUG: print(response)
+            embed=discord.Embed(description=bot_utils.sanitize_input(response))
+            embed.set_footer(text=f"?{command[0]} requested by: {ctx.author}")
+            embed.set_image(url=command_type)
+            await ctx.send(embed=embed)
+            if delete:
+                await ctx.message.delete()
+
+    def get_command_name(self, ctx):
+        command = ctx.message.content.split()[0][1:]
+        command = command.replace("-", " ")
+        command = command.replace("_", " ")
+        command = " ".join([i.capitalize() for i in command.split()])
+        return command
 
     async def add_command(self, ctx, command_type, command, response):
         """
         Adds a command to the database.
         """
 
-        if not await bot_utils.await_confirm(ctx, F"Create `?{command}`?\nPlease ensure that the command is inline with the command creation guidlines. View guidlines with `?command_creation`"):
+        await self.send_command(ctx, response, command="?PREVIEW", delete=False)
+        if not await bot_utils.await_confirm(ctx, F"Create `?{command}`?\nPlease ensure that the command is inline with the command creation guidlines. View guidlines with `?command_creation`", confirm_time=60):
             await ctx.send("Command wasn't created.")
             return
 
-        approval_message = await ctx.send("Thanks for your new command. It has been submitted for approval and should be live soon!")
-        if not await bot_utils.await_mod_confirm(ctx, f"**A command creation requires approval:**\nCommand: `?{command}`\nLink: {ctx.message.jump_url}", delete_after=False, confirm_time=21000):
-            await approval_message.edit(content=f"{ctx.author.mention} The command was not approved.")
-            return
-        else:
-            await approval_message.delete()
+        if self.config_data['require_approval'] == "True":
+            approval_message = await ctx.send("Thanks for your new command. It has been submitted for approval and should be live soon!")
+            if not await bot_utils.await_mod_confirm(ctx, f"**A command creation requires approval:**\nCommand: `?{command}`\nLink: {ctx.message.jump_url}", delete_after=False, confirm_time=21000):
+                await approval_message.edit(content=f"{ctx.author.mention} The command was not approved.")
+                return
+            else:
+                await approval_message.delete()
 
         try:
             # ADD COMMAND TO THE DATABASE
@@ -309,7 +371,7 @@ class CommandsDB(commands.Cog):
             self.c.execute("INSERT INTO Commands(command, response, command_type, owner, timestamp) VALUES (?, ?, ?, ?, ?)", (command.strip('```'), response, command_type, ctx.author.id, datetime.datetime.utcnow()))
             self.conn.commit()
             await ctx.send(f"{ctx.author.mention} New {command_type} command: '{command}'")
-            await self.send_command(ctx, response, delete=False)
+            # await self.send_command(ctx, response, delete=False)
 
         except:
             # REPORT ERROR
@@ -346,11 +408,15 @@ class CommandsDB(commands.Cog):
 
         # CREATE PAGINATOR
         paginator = commands.Paginator(prefix='```\n', suffix='\n```')
-        paginator.add_line(f'--- DATABASE COMMANDS ({len(result)}) ---')
+        paginator.add_line(f'--- DATABASE COMMANDS (Aliases) ({len(result)}) ---')
 
         # ADD COMMANDS TO PAGINATOR
         for command in result:
-            paginator.add_line(command[0])
+            self.c.execute("SELECT * FROM Commands WHERE command_type=? AND response=?", ("alias", command[0]))
+            aliases = self.c.fetchall()
+            if DEBUG: print(aliases)
+            output_string = ", ".join([i[0] for i in aliases])
+            paginator.add_line(f"{command[0]} ({output_string})")
 
         # SEND PAGINATOR
         for page in paginator.pages:
@@ -359,7 +425,7 @@ class CommandsDB(commands.Cog):
     async def did_you_mean(self, ctx, input_str):
 
         # GET ALL COMMANDS
-        self.c.execute("SELECT * FROM Commands")
+        self.c.execute("SELECT * FROM Commands WHERE command_type='help'")
         commands = self.c.fetchall()
 
         # FIND POSSIBLE MATCHES
