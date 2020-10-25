2bcb2a1f96307831f697b180238383ff6641c6f5
root <root@test.edc.com>
2020-10-25 22:27:17 +0100

    Final Code Module

--- modules/Code/Code.py
+++ modules/Code/Code.py
@@ -15,25 +15,6 @@ class Code(commands.Cog):
         # with open('modules/Code/config.json') as f:
         #     self.config_data = json.load(f)
 
-    @commands.Cog.listener()
-    async def on_message(self, message): 
-        if message.author.id == self.bot.user.id:
-            return
-
-        if not message.channel.id == 339978089411117076:
-            return
-
-        print("Message received")
-        if re.search('[G|M][0-9]', message.content) and not '```gcode' in message.content:
-            gcode = []
-            for l in message.content.split('\n'):
-                if l.startswith(';') or re.search('^[G|M][0-9]', l):
-                    gcode.append(l)
-
-            if len(gcode) > 0:
-                gcode_out = "\n".join(gcode)
-                await message.channel.send(f"```gcode\n{gcode_out}\n```")
-
     @commands.command(aliases=['g'])
     async def gcode(self, ctx):
         await self.send_gcode(ctx.message, command=f"{ctx.prefix}{ctx.invoked_with}")
