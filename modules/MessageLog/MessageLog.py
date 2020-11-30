65079a53ea7819049e279d63acb621d4f047959e
root <root@test.edc.com>
2020-11-30 15:52:44 +0100

    shows diff

--- modules/MessageLog/MessageLog.py
+++ modules/MessageLog/MessageLog.py
@@ -2,7 +2,27 @@
 import discord
 from discord.ext import commands
 import json
+import difflib
 import bot_utils
+
+def diff(a, b ):
+
+    sm = difflib.SequenceMatcher(None, a, b)
+    
+    output= []
+    for opcode, a0, a1, b0, b1 in sm.get_opcodes():
+        if opcode == 'equal':
+            output.append(sm.a[a0:a1])
+        elif opcode == 'insert':
+            output.append("**" + sm.b[b0:b1] + "**")
+        elif opcode == 'delete':
+            pass
+            # output.append("**" + sm.a[a0:a1] + "**")
+        elif opcode == 'replace':
+            output.append("**" + sm.b[b0:b1] + "**")
+        else:
+            raise RuntimeError
+    return ''.join(output)
 
 class MessageLog(commands.Cog):
     version = "v0.1"
@@ -12,11 +32,21 @@ class MessageLog(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message_edit(self, before, after):
-        await bot_utils.log(self.bot, title='Message Edited', color=bot_utils.yellow, From=f"{before.author.mention} [{before.author}]", Before=before.content[:1000], After=after.content[:1000], channel=500395384720588810, DM=False)
+        if before.author == self.bot.user:
+            return
+
+        if before.content == after.content:
+            return
+
+        result = diff(before.content, after.content)
+        await bot_utils.log(self.bot, title='Message Edited', color=bot_utils.yellow, From=f"{before.author.mention} [{before.author}]", Before=before.content, After=result, channel=500395384720588810, DM=False)
 
     @commands.Cog.listener()
     async def on_message_delete(self, message):
-        await bot_utils.log(self.bot, title='Message Deleted', color=bot_utils.yellow, From=f"{message.author.mention} [{message.author}]", Before=message.content[:1000], channel=500395384720588810, DM=False)
+        if message.author == self.bot.user:
+            return
+
+        await bot_utils.log(self.bot, title='Message Deleted', color=bot_utils.red, From=f"{message.author.mention} [{message.author}]", Message=message.content[:1000], channel=500395384720588810, DM=False)
 
 def setup(bot):
     bot.add_cog(MessageLog(bot))
