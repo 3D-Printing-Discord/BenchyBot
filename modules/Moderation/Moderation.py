2c0234dc313795fcd87874048daddb446cf6ec9f
EdwardChamberlain <edward.chamberlain@me.com>
2020-12-31 13:42:28 +0100

    switched database handler, aliases, ban reason

--- modules/Moderation/Moderation.py
+++ modules/Moderation/Moderation.py
@@ -23,10 +23,11 @@ class Moderation(commands.Cog):
     async def on_member_ban(self, guild, user):
         await bot_utils.log(self.bot, title="Member Banned", Member=user, color=bot_utils.red)
 
-    @commands.command()
+    @commands.command(aliases=['warn'])
     @commands.has_any_role(*bot_utils.admin_roles)
     async def rule(self, ctx, rule_number, member: discord.Member=None):
-        '''Shows a server rule.'''
+        '''Shows a server rule or issues a warning.'''
+
         await ctx.message.delete()
 
         embed = discord.Embed(title=f"Rule {rule_number}", description=self.config_data['rules'][rule_number], color=bot_utils.red)
@@ -34,18 +35,23 @@ class Moderation(commands.Cog):
         
         if member:
             embed.set_author(name=f"{member} please see the rule below:")
+
             warn_mesg = await ctx.send(member.mention, embed=embed)
-            self.c.execute("INSERT INTO Moderation_warnings(timestamp, user_id, reason, jump_link) VALUES (?, ?, ?, ?)", (datetime.datetime.utcnow(), member.id, rule_number, warn_mesg.jump_url))
-            self.conn.commit()
+
+            self.bot.databasehandler.sqlquery(
+                "INSERT INTO Moderation_warnings(timestamp, user_id, reason, jump_link) VALUES (?, ?, ?, ?)",
+                datetime.datetime.utcnow(), member.id, rule_number, warn_mesg.jump_url,
+                return_type='commit'
+            )
 
             log_embed = discord.Embed(title="Warning Issued", color=bot_utils.red)
             log_embed.set_footer(text=f"Issued by: {ctx.author}")
-
             log_embed.add_field(name="Issued to", value=f"{member} [{member.mention}]", inline=False)
             log_embed.add_field(name="Reason", value=f"Rule {rule_number}", inline=False)
             log_embed.add_field(name="Link", value=f"[Jump link]({warn_mesg.jump_url})", inline=False)
 
             await ctx.guild.get_channel(self.bot.config['bot_log_channel']).send(embed=log_embed)
+
         else:
             await ctx.send(embed=embed)
 
@@ -61,18 +67,15 @@ class Moderation(commands.Cog):
             await ctx.author.send("Warning was not issued: Could not find the user.")
             ctx.handled_in_local = True
 
-    @commands.command()
+    @commands.command(aliases=['warns'])
     @commands.has_any_role(*bot_utils.admin_roles)
     async def show_infractions(self, ctx, *, member):
         '''Shows the number and type of infractions a user has collected.'''
 
-        banned_string = False
-
         try:
             member = await commands.MemberConverter().convert(ctx, member)
-        except:
-            bans = await ctx.guild.bans()
-            
+            banned_string = False
+        except:            
             try:
                 ban_entry = [i for i in await ctx.guild.bans() if i.user.name == member][0]
                 member = ban_entry.user
@@ -81,18 +84,18 @@ class Moderation(commands.Cog):
                 await ctx.send("Member Not Found!")
                 return
         
-        self.c.execute("SELECT * FROM Moderation_warnings WHERE user_id=?", (member.id,))
-        results = self.c.fetchall()
+        results = self.bot.databasehandler.sqlquery(
+            "SELECT * FROM Moderation_warnings WHERE user_id=?",
+            member.id,
+            return_type='all'
+        )
         
-        inf = {'1':0, '2':0, '3':0, '4':0, '5':0, '6':0, '7':0}
-        for v in results:
-            inf[v[2]] += 1
+        warnings = {'1':0, '2':0, '3':0, '4':0, '5':0, '6':0, '7':0}
+        for i in results:
+            warnings[i[2]] += 1
 
-        output = [f"Rule {k}: {v}" for k, v in inf.items() if v != 0]
-        out_string = "\n".join(output)
-
-        out_list = [f"[Rule: {i[2]}]({i[3]})" for i in results]
-        out_list_string = '\n'.join(out_list)
+        out_string = "\n".join([f"Rule {k}: {v}" for k, v in warnings.items() if v != 0])
+        out_list_string = '\n'.join([f"[Rule: {i[2]}]({i[3]})" for i in results])
 
         embed = discord.Embed(title=f"Infractions for {member.name}", color=bot_utils.red)
         if banned_string:
@@ -121,11 +124,7 @@ class Moderation(commands.Cog):
                 embed.add_field(name="Content", value=message.clean_content, inline=False)
             embed.add_field(name="Attachments", value=len(message.attachments), inline=False)
 
-            try:
-                await self.bot.get_guild(RawReactionActionEvent.guild_id).get_channel(self.bot.config['bot_log_channel']).send(embed=embed, files=[await i.to_file() for i in message.attachments])
-            except:
-                await self.bot.get_guild(RawReactionActionEvent.guild_id).get_channel(self.bot.config['bot_log_channel']).send("**An Error Occured!**\nAn error orrured reporting a post deletion. You shouldnt be seeing this message.")
-
+            await self.bot.get_guild(RawReactionActionEvent.guild_id).get_channel(self.bot.config['bot_log_channel']).send(embed=embed, files=[await i.to_file() for i in message.attachments])
             await message.delete()  
 
 def setup(bot):
